db0ab10b7a3efd13a585c6366c262008c51a85e8
KUNJAN PATEL <kunjan@ucla.edu>
2019-02-15 15:25:36 -0800

    Add files via upload

---
+++ Q1/README.md
@@ -0,0 +1,25 @@
+## Description
+### Files
+- q1.py
+
+### Code
+**q1.py** <br>
+- function `def equalsWhenOneCharRemoved(x, y)`
+  - Parameters
+    - x string 1
+    - y string 2
+  - Output/Return Bool
+    - True if possible
+    - False
+- function `def Test(test_set = global_test_set, result_set=global_result_set)`
+  - Assertion based Testing function Parameters:
+    - Test set : list of tuples(string1 , string2)
+    - Result_set: list of return bools
+    - Defaults:
+      - [Test Set](https://github.com/coolkp/moloco/blob/master/Q1/q1.py#L1)
+      - [Result_set](https://github.com/coolkp/moloco/blob/master/Q1/q1.py#L2)
+- Complexity
+  - Runtime
+    - Parameters s1 of length n and s2 of length m where |m-n| = 1
+    - Worst case O(max(m,n))
+    
---
+++ Q1/q1.py
@@ -0,0 +1,28 @@
+global_test_set = [("",""),("", "x"),("x", ""),("x", "x"),("abcd", "eabcd"),("abcd", "aebcd"),("abcd", "abcde"),("xyz", "xz"),("yxz", "xz")]
+global_result_set =[False,True,True,False,True,True,True,True,True]
+def equalsWhenOneCharRemoved(x, y):
+    if abs(len(x) -len(y)) != 1:
+        return False
+    i = 0
+    j = 0
+    skip = 0
+    while(i<len(x) and j < len(y)):
+        if x[i] != y[j]:
+            skip += 1
+            if(skip > 1):
+                return False
+            if len(x) > len(y):
+                i += 1
+            else:
+                j += 1
+        else:
+            i +=1
+            j += 1
+    return True
+def Test(test_set = global_test_set,result_set=global_result_set ):
+    i = 0
+    for test in test_set:
+        assert equalsWhenOneCharRemoved(test[0],test[1]) == result_set[i], "Incorrect Result %s,%s" % test
+        i+=1
+    print(len(test_set),"Tests passed successfully")
+Test()
